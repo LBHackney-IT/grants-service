642828ded4aeaee06d9292b0042b59873b51a143
mattbee <matt.bee@madetech.com>
2020-12-16 19:50:37 +0000

    Update order of Application States
    To make the statuses easier to use and make more sense with the similar
    states closer to each other, this commit updates the order of the
    statuses.
    
    NOTE: The only currently used states on the production site are
    "Declined for test" and "Unprocessed" which is why this change is
    possible and they are the only states updated in `application_assessment`
    in the migrations. It is not advised to change any other states in
    other live applications where the states are already in use.

---
+++ db/migrations/sqls/20201216192624-reorder-states-down.sql
@@ -0,0 +1,38 @@
+UPDATE application_state
+SET description = 'Unprocessed' WHERE id = 1;
+
+UPDATE application_state
+SET description = 'Pre-processed' WHERE id = 2;
+
+UPDATE application_state
+SET description = 'Pre-processed - Review' WHERE id = 3;
+
+UPDATE application_state
+SET description = 'Processed - Approved' WHERE id = 4;
+
+UPDATE application_state
+SET description = 'Processed - Rejected' WHERE id = 5;
+
+UPDATE application_state
+SET description = 'NFI' WHERE id = 6;
+
+UPDATE application_state
+SET description = 'Refer to Manager' WHERE id = 7;
+
+UPDATE application_state
+SET description = 'Closed - Duplicate' WHERE id = 8;
+
+UPDATE application_state
+SET description = 'Exported for Payment' WHERE id = 9;
+
+UPDATE application_state
+SET description = 'Declined - Test' WHERE id = 10;
+
+UPDATE application_state
+SET description = 'Processed - Check Bank Details' WHERE id = 11;
+
+UPDATE application_state
+SET description = 'Tier 2 ARG application' WHERE id = 12;
+
+UPDATE application_assessment
+SET application_state_id = 10 WHERE application_state_id = 12;
