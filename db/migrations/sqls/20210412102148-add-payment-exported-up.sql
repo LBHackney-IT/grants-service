39f670430b4663f508017cb6c69962b322d10c1b
Kat <katrina@madetech.com>
2021-04-13 09:33:54 +0100

    Mark payment exported conditionally

--- db/migrations/sqls/20210412102148-add-payment-exported-up.sql
+++ db/migrations/sqls/20210412102148-add-payment-exported-up.sql
@@ -5,7 +5,9 @@ ALTER TABLE application_assessment
 ADD round_2_payment_exported boolean DEFAULT false;
 
 UPDATE application_assessment
-SET round_1_payment_exported = true;
+SET round_1_payment_exported = true
+WHERE application_state_id = 8 AND grant_amount_awarded <> 0;
 
 UPDATE application_assessment
-SET round_2_payment_exported = true;
+SET round_2_payment_exported = true
+WHERE application_state_id = 8 AND grant_amount_awarded_round_2 <> 0;
