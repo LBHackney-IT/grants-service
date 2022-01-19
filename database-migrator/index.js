2fb416aa5697caca24f3bf9c2ccde1380788f5ce
Owen Pigott <owen.pigott@madetech.com>
2022-01-19 15:29:17 +0000

    Database configuration, for purposes of migration

--- database-migrator/index.js
+++ database-migrator/index.js
@@ -3,7 +3,7 @@ const exec = promisify(require('child_process').exec);
 
 module.exports.handler = async () => {
   const output = await exec(
-    "node_modules/.bin/db-migrate --migrations-dir 'db/migrations/' up"
+    "node_modules/.bin/db-migrate --migrations-dir 'db/migrations/' up --config database.json -e production"
   );
 
   console.log(output.stdout);
