ad603550f2b10a69c0e7c9bccb6b1b1390a3d473
Owen Pigott <owen.pigott@madetech.com>
2022-01-19 15:04:17 +0000

    Changed log to warn to show up on screen in deployment

--- database-migrator/index.js
+++ database-migrator/index.js
@@ -5,8 +5,8 @@ module.exports.handler = async () => {
   const output1 = await exec('ls node_modules/.bin/');
   const output2 = await exec('ls db/migrations/');
 
-  console.log(output1.stdout);
-  console.log(output2.stdout);
+  console.warn(output1.stdout);
+  console.warn(output2.stdout);
 
   const output = await exec(
     "node_modules/.bin/db-migrate --migrations-dir 'db/migrations/' up"
