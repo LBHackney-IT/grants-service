9f33021d8b89e5b75a0c48de1e24283043147159
Owen Pigott <owen.pigott@madetech.com>
2022-01-19 15:20:45 +0000

    Include database config file in database migration lambda

--- database-migrator/index.js
+++ database-migrator/index.js
@@ -2,12 +2,6 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
 module.exports.handler = async () => {
-  const output1 = await exec('ls node_modules/.bin/');
-  const output2 = await exec('ls db/migrations/');
-
-  console.warn(output1.stdout);
-  console.warn(output2.stdout);
-
   const output = await exec(
     "node_modules/.bin/db-migrate --migrations-dir 'db/migrations/' up"
   );
