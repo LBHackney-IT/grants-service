fa53a4bca16c8b0191ec3bac56935eaee02af28b
Chris Hutchinson <chris_hutchinson@me.com>
2022-01-19 13:48:48 +0000

    feat: first pass at a database migrator lambda

---
+++ database-migrator/index.js
@@ -0,0 +1,14 @@
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+module.exports.handler = async () => {
+  const output = await exec(
+    "node_modules/.bin/db-migrate --migrations-dir 'db/migrations/' up"
+  );
+
+  console.log(output.stdout);
+
+  if (output.stderr) console.error(output.stderr);
+
+  return true;
+};
