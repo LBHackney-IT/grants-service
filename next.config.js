56eeee642e3247f2c167cfc6f3130816fc0072b1
Chris Hutchinson <chris_hutchinson@me.com>
2022-01-21 09:56:24 +0000

    Remove powered by header, set frame option, XSS and content type options headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 module.exports = {
   distDir: 'build/_next',
   target: 'server',
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
 };
