03ec8fd8db39298566e516bef12a94f90ab2f617
Chris Hutchinson <chris_hutchinson@me.com>
2022-01-21 11:52:34 +0000

    More specific test assertion

--- grants/grants.test.ts
+++ grants/grants.test.ts
@@ -1,9 +1,10 @@
+import { NotFoundError } from '../utils/errors';
 import { getGrantBySlug } from './grants';
 
 describe('grants', () => {
   describe('#getGrantBySlug', () => {
     it('should throw if an unknown slug is proviuded', () => {
-      expect(() => getGrantBySlug('unknown-grant')).toThrow();
+      expect(() => getGrantBySlug('unknown-grant')).toThrow(NotFoundError);
     });
 
     it('should return a grant with a valid slug', () => {
