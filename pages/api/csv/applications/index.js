ea779e34b9127d1c5ef088489899bb2fcf0a7052
Chris Hutchinson <chris_hutchinson@me.com>
2022-01-25 17:29:24 +0000

    feat: download grant applications CSV by type

--- pages/api/csv/applications/index.js
+++ pages/api/csv/applications/index.js
@@ -5,15 +5,22 @@ export default async (req, res) => {
   switch (req.method) {
     case 'GET':
       try {
+        const grantType = req.query.grantType;
+        if (!grantType) {
+          throw new Error("Missing 'grantType' parameter");
+        }
+
         const container = AppContainer.getInstance();
         const listApplicationsCSV = container.getListApplicationsCSV();
         res.statusCode = HttpStatus.OK;
         res.setHeader('Content-Type', 'text/csv; charset=utf-8');
         res.setHeader('Content-Disposition', 'filename=data.csv');
-        const csvResult = await listApplicationsCSV();
+        const csvResult = await listApplicationsCSV({
+          grantType,
+        });
         res.end(csvResult.csvString);
       } catch (error) {
-        console.log('Applications CSV error:', error, 'request:', req);
+        console.log('Applications CSV error:', error);
         res.statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
         res.end(JSON.stringify('Unable to generate an applications CSV'));
       }
