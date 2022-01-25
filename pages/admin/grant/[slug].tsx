38120d567f462f29c20bfe6e9ee50d3f595bc621
Chris Hutchinson <chris_hutchinson@me.com>
2022-01-25 14:10:23 +0000

    Fix existing integration tests

--- pages/admin/grant/[slug].tsx
+++ pages/admin/grant/[slug].tsx
@@ -30,7 +30,9 @@ const AdminManageGrantPage = (props) => {
         </ol>
       </div>
 
-      <span className="govuk-caption-xl">Hello {props.name}</span>
+      <span className="govuk-caption-xl" data-testid="admin-page-subheading">
+        Hello {props.name}
+      </span>
       <h1 className="govuk-heading-xl">{grant.name}</h1>
 
       <ApplicationsList {...props} grantType={slug} grantName={grant.name} />
