512342a864830fe431389dfac5d33769e09ac638
Owen Pigott <owen.pigott@madetech.com>
2022-03-24 13:20:14 +0000

    Reverted as chunked encoding not working on deployed (lambda) env

--- pages/api/applications/[clientGeneratedId]/document/[s3Path].js
+++ pages/api/applications/[clientGeneratedId]/document/[s3Path].js
@@ -1,16 +1,31 @@
 /* eslint-disable no-case-declarations */
 import * as HttpStatus from 'http-status-codes';
 import { signedUrl } from '../../../../../lib/usecases/getSignedDocumentUrl';
-import request from 'request';
+import fetch from 'node-fetch';
+import { mimeType } from '../../../../../utils/mimeTypes';
 
 export default async (req, res) => {
   switch (req.method) {
     case 'GET':
       const s3Path = req.query.s3Path;
+      const filename = s3Path.split('/').slice(-1)[0];
+      const fileType = mimeType(s3Path);
 
       try {
         const url = await signedUrl({ s3Path });
-        return request.get(url).pipe(res);
+        const response = await fetch(url);
+
+        if (!response.ok)
+          throw new Error(`unexpected response ${response.statusText}`);
+
+        console.log(response);
+        res.setHeader('Content-Type', fileType);
+        res.setHeader(
+          'Content-Disposition',
+          'attachment; filename=' + filename
+        );
+
+        res.send(response.body);
       } catch (error) {
         console.log('Document Signed URL error:', error, 'request:', req);
         res.statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
