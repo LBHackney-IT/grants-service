5648ed800a3dd873dc210e488164f94140b2bf80
Owen Pigott <owen.pigott@madetech.com>
2022-03-24 08:07:38 +0000

    Change URL to be acquired by server

--- pages/api/applications/[clientGeneratedId]/document/[s3Path].js
+++ pages/api/applications/[clientGeneratedId]/document/[s3Path].js
@@ -1,16 +1,35 @@
+/* eslint-disable no-case-declarations */
 import * as HttpStatus from 'http-status-codes';
 import { signedUrl } from '../../../../../lib/usecases/getSignedDocumentUrl';
+import stream from 'stream';
+import { promisify } from 'util';
+import fetch from 'node-fetch';
+import { mimeType } from '../../../../../utils/mimeTypes';
+
+const pipeline = promisify(stream.pipeline);
 
 export default async (req, res) => {
   switch (req.method) {
     case 'GET':
-      // eslint-disable-next-line no-case-declarations
       const s3Path = req.query.s3Path;
+      const filename = s3Path.split('/').slice(-1)[0];
+      const fileType = mimeType(s3Path);
+
       try {
-        res.writeHead(HttpStatus.MOVED_TEMPORARILY, {
-          Location: await signedUrl({ s3Path }),
-        });
-        res.end();
+        const url = await signedUrl({ s3Path });
+        const response = await fetch(url);
+
+        if (!response.ok)
+          throw new Error(`unexpected response ${response.statusText}`);
+
+        console.log(response);
+        res.setHeader('Content-Type', fileType);
+        res.setHeader(
+          'Content-Disposition',
+          'attachment; filename=' + filename
+        );
+
+        await pipeline(response.body, res);
       } catch (error) {
         console.log('Document Signed URL error:', error, 'request:', req);
         res.statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
